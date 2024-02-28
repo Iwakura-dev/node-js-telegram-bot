2bd6a5c77f71eb6514af2b335cca974e9368ac62
Iwakura-dev <escayhokage@gmail.com>
2024-02-28 11:25:12 -0800

    fix: 🐛 dev
    remove build folders in dev branch

--- build/constants/constants.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var _a, _b, _c;
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ko_fi = exports.buyMeCoffee = exports.token = void 0;
-require("dotenv/config");
-exports.token = (_a = process.env.API_TOKEN_TELEGRAM_BOT) !== null && _a !== void 0 ? _a : '';
-exports.buyMeCoffee = (_b = process.env.URL_BY_ME_A_COFFEE) !== null && _b !== void 0 ? _b : '';
-exports.ko_fi = (_c = process.env.URL_KO_FI) !== null && _c !== void 0 ? _c : '';
