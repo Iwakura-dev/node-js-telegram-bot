f964cf16787af4cb13fe8df4a9a256ef4a734aad
Iwakura-dev <escayhokage@gmail.com>
2024-02-28 17:30:37 -0800

    fix: 🐛 code
    fix app.ts

--- build/app.js
+++ build/app.js
@@ -1,12 +1,12 @@
-import 'dotenv/config';
 import express from 'express';
 import { webhookCallback } from 'grammy';
 import { bot } from './bot/bot.js';
 const domain = String(process.env.DOMAIN);
-const secretPath = String(process.env.API_TOKEN_TELEGRAM_BOT);
+const secretPath = String(process.env.BOT_TOKEN);
 const app = express();
 app.use(express.json());
 app.use(`/${secretPath}`, webhookCallback(bot, 'express'));
 app.listen(Number(process.env.PORT), async () => {
+    // Make sure it is `https` not `http`!
     await bot.api.setWebhook(`https://${domain}/${secretPath}`);
 });
--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,15 @@
-import 'dotenv/config';
 import express from 'express';
 import { webhookCallback } from 'grammy';
 import { bot } from './bot/bot';
 
 const domain = String(process.env.DOMAIN);
-const secretPath = String(process.env.API_TOKEN_TELEGRAM_BOT);
-const PORT = process.env.PORT || 3000;
+const secretPath = String(process.env.BOT_TOKEN);
 const app = express();
 
 app.use(express.json());
 app.use(`/${secretPath}`, webhookCallback(bot, 'express'));
 
-app.listen(Number(PORT), async () => {
-  await bot.api.deleteWebhook();
+app.listen(Number(process.env.PORT), async () => {
+  // Make sure it is `https` not `http`!
   await bot.api.setWebhook(`https://${domain}/${secretPath}`);
 });
