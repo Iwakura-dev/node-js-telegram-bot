428fc06f2e21602325627097306fc18c66056e2b
Iwakura-dev <escayhokage@gmail.com>
2024-02-28 16:38:19 -0800

    fix: 🐛 port
    add check in port

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,12 @@ import { bot } from './bot/bot';
 
 const domain = String(process.env.DOMAIN);
 const secretPath = String(process.env.API_TOKEN_TELEGRAM_BOT);
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
 app.use(`/${secretPath}`, webhookCallback(bot, 'express'));
 
-app.listen(Number(process.env.PORT), async () => {
+app.listen(PORT, async () => {
   await bot.api.setWebhook(`https://${domain}/${secretPath}`);
 });
